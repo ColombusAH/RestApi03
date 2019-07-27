5cd66e2d597dd83f23599225c9c7e9ffa14eea1a
Avner <Hattabavner@gmail.com>
2019-07-27 20:33:03 +0300

    Joi validation added

--- src/middlewares/validatesMiddlewares.ts
+++ src/middlewares/validatesMiddlewares.ts
@@ -1,14 +1,27 @@
 import { Request, Response, NextFunction } from "express";
-
 import { InputValidationError } from "../errors/HttpErrors";
+import Joi from "joi";
 
+const idValidationSchema = Joi.object().keys({
+  id: Joi.string()
+    .regex(
+      /^[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}$/
+    )
+    .required()
+});
+const nameValidationSchema = Joi.object().keys({
+  name: Joi.string()
+    .min(3)
+    .regex(/^[a-zA-Z]/)
+    .required()
+});
 export function validateNameLength(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  const name: string = req.body.name;
-  if (!name || name.length < 3) {
+  const result = Joi.validate({ name: req.body.name }, nameValidationSchema);
+  if (result.error) {
     throw new InputValidationError(`The name length need to be 3 at least`);
   } else {
     next();
@@ -20,8 +33,9 @@ export function validateIdLength(
   res: Response,
   next: NextFunction
 ) {
-  const id = req.params.id;
-  if (!id || id.length !== 36) {
+  const result = Joi.validate({ id: req.params.id }, idValidationSchema);
+  console.log(result);
+  if (result.error) {
     throw new InputValidationError("The id length must 36");
   } else {
     next();
